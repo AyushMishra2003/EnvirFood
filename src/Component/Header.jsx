01d3f3596f30fa384c60e4395062a1baa183ba9a
AYUSH MISHRA <ayushm185@gmail.com>
2024-04-06 13:05:16 +0530

    changes updated

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -19,10 +19,10 @@ const Header = () => {
       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] shadow  w-[100vw] transition-[2s] flex flex-col items-center gap-[2.5rem]  absolute top-[-2rem] left-[-1rem]  bg-black ">
        <div className='flex justify-center items-center gap-[2rem] '> 
          <img  className='w-[4rem]'  src={logo} alt="ecologo" />
-         <p className='text-[1.6rem] font-a'>Environmental Food Pvt Ltd</p>
+         <p className='sm:text-[1.6rem] text-[1.2rem] font-a'>Environmental Food Pvt Ltd</p>
         </div> 
         <div className='w-[100vw] flex flex-col items-center justify-center gap-3'>
-        <li className='border-t border-white w-[100vw] items-center'><a className='text-[1.2rem]  border-b-1 '>Home</a></li>
+        <li className='border-t w-[100vw] items-center'><a className='text-[1.2rem]  border-b-1 '>Home</a></li>
         <li className='border-t border-white w-[100vw] items-center'>
         <details className='w-[15rem] flex flex-col items-center border-t border-white'>
           <summary className='text-[1rem] text-[#08A05E] p-[0.2rem] px-[2rem] border border-white'>What We Do</summary>
@@ -34,15 +34,16 @@ const Header = () => {
           </ul>
         </details>
       </li>
-        <li className='border-t border-white w-[100vw] items-center'><a className='text-[1.2rem]'>Contact Us</a></li>
-        <li className='border-t border-white w-[100vw] items-center'><a className='text-[1.2rem]'>About Us</a></li>
-        <li className='border-t border-white w-[100vw] items-center'><a className='text-[1.2rem]'>Application</a></li>
+      <Link to={"/Contact"}><li className='text-[1rem]'><a>Contact Us</a></li></Link>
+      <Link to={"/About"}><li className='text-[1rem]'><a>About Us</a></li></Link>
+      <Link to={"/"}><li className='text-[1rem]'><a>Application</a></li></Link>
+        {/* <li className='border-t border-white w-[100vw] items-center'><a className='text-[1.2rem]'>Application</a></li> */}
         <li className='border-t border-white w-[100vw] items-center'><a className='text-[1.2rem]'>Industry</a></li>
         <li className='border-t border-white w-[100vw] items-center'><a className='text-[1.2rem]'>Insight</a></li>
         </div>
       </ul>
     </div>
-       <div className="border border-white rounded-md flex items-center justify-center gap-3 p-2">
+       <div className="rounded-md flex items-center justify-center gap-3 p-2">
          <img  className='w-[4rem]'  src={logo} alt="ecologo" />
         <p className='text-[2rem] hidden lg:block font-curve font-bold' >Environmental Food Pvt Ltd</p>
        </div>
@@ -55,7 +56,7 @@ const Header = () => {
         <details>
           <summary className='text-[1rem] text-[#08A05E]'>What We Do</summary>
           <ul className="p-2  w-[20rem] flex flex-col items-center justify-center bg-[#d1d1d1]">
-            <li><a className='text-[1.2rem] text-[#08A05E]'>Food Development</a></li>
+            <li ><a className='text-[1.2rem] text-[#08A05E]'>Food Development</a></li>
             <li><a className='text-[1.2rem] text-[#08A05E]'>Food Manufacturing</a></li>
             <li><a className='text-[1.2rem] text-[#08A05E]'>Sea Food Development</a></li>
             <li><a className='text-[1.2rem] text-[#08A05E]'>Nutrition Intelligence</a></li>
